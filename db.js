ca6bdbbae3b00c83e538eb76fabc0eb2484bb703
samuelhirsch <shmielhirsch@gmail.com>
2026-05-12 16:06:43 -0400

    3

--- db.js
+++ db.js
@@ -2,15 +2,13 @@ import mysql from 'mysql2/promise';
 
 // Create connection pool
 
-
-const connection = await mysql.createPool({
+const db = await mysql.createPool({
   host: process.env.MYSQL_HOST,
   port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE
 });
-
 // Create table
 await db.execute(`
   CREATE TABLE IF NOT EXISTS synced_expenses (
