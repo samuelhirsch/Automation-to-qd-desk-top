3642f2e8d9c62fa2e79dda9166b68bee31b02e90
samuelhirsch <shmielhirsch@gmail.com>
2026-05-12 15:37:17 -0400

    2

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
 import mysql from 'mysql2/promise';
 
 // Create connection pool
-const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '', // add your password if you have one
-  database: 'quickbookstest'
+
+
+const connection = await mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
 });
 
 // Create table
